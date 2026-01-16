128bb72e1da3da9913190854eb5df7ab0a9db2f2
jessfig <jessicafigueiredopereira@gmail.com>
2026-01-16 16:49:41 -0300

    adicionando escrita de arquivos em parquet

--- app/scripts/generate_parquet/generate_parquet.py
+++ app/scripts/generate_parquet/generate_parquet.py
@@ -1,30 +1,45 @@
+from pathlib import Path
 from pyspark.sql import SparkSession
-from app.scripts.enums.enum_tpch_tables import TablesTPCH
-from app.scripts.enums.enum_tpch_scale_factor import ScaleFactorTPCH
+from enums.enum_tpch_tables import TablesTPCH
+from enums.enum_tpch_scale_factor import ScaleFactorTPCH
+from schema_files import SchemaFiles
 
 
 class ConvertToParquet:
     def __init__(self):
         self.spark = SparkSession.builder.appName("ConvertToParquet").getOrCreate()
+        self.schemas = SchemaFiles()
         self.input_path = '/data/tpch'
-        self.output_path = '/data/tpch/parquet'
+        self.output_path = '/data/tpch/tpch_parquet'
 
     def convert_files(self, scale_factor: float):
         for table in TablesTPCH:
-            print(f"Iniciando conversão da tabela {table} para parquet!")
+            print(f"Iniciando conversão da tabela {table.value} para parquet!")
             df = self.__read_file(table.value, scale_factor)
             self.__write_file(df, scale_factor)
-            print(f"Finalizando conversão da tabela {table} para parquet!")
+            print(f"Finalizando conversão da tabela {table.value} para parquet!")
 
     def __read_file(self, table: str, scale_factor: float):
         input_file = f"{self.input_path}/sf{scale_factor}/{table}.tbl"
-        df = self.spark.read.option("sep", "|").option("inferSchema", "true").csv(input_file)
-        # Deletando ultima coluna que é nula
-        df = df.drop(df.columns[-1])
+        schema = self.__get_schema(table)
+        df = self.spark.read.option("delimiter", "|").option("dateFormat", "yyyy-MM-dd").schema(schema).csv(input_file)
         return df
 
     def __write_file(self, df, scale_factor: float):
-        df.write.mode("overwrite").parquet(f"{self.output_path}/sf{scale_factor}")
+        path = f"{self.output_path}/sf{scale_factor}"
+        output_path = Path(path)
+        output_path.mkdir(parents=True, exist_ok=True)
+        df.write.mode("overwrite").parquet(path)
+
+    def __get_schema(self, table:str):
+        strategies = {
+            TablesTPCH.ITENS_PEDIDOS.value: self.schemas.get_lineitem_schema,
+            TablesTPCH.CLIENTES.value: self.schemas.get_customer_schema,
+            TablesTPCH.PAISES.value: self.schemas.get_nation_schema,
+            TablesTPCH.PEDIDOS.value: self.schemas.get_orders_schema
+        }
+        strategie = strategies.get(table)
+        return strategie()
 
 
 if __name__ == "__main__":
